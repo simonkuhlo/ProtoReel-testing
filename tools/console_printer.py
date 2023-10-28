f32a08d5de67f06c2a5e5a93abb871ffac938ba4
Simon <siga241@gmail.com>
2023-10-28 21:02:48 +0200

    create console_printer

--- tools/console_printer.py
+++ tools/console_printer.py
@@ -1,10 +1,21 @@
-if __name__ == "tools/console_printer":
-    from res import config_handler
-
 if __name__ == "__main__":
     import sys
+    import asyncio
     sys.path.append(".") 
-    from res import config_handler
+from res import config_handler
+import termcolor
 
-config = config_handler.get_config("term_formatting")
-print(config)
+
+async def get_status_config():
+    config = config_handler.get_config("term_formatting")
+    status_config = config["status_codes"]
+    return(status_config)
+
+async def print_status(status, message):
+    return
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(get_status_config())
