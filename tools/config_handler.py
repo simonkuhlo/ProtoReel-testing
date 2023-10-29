f889d1e27914feb8a3ef275d522cede0cfe69cef
Simon <siga241@gmail.com>
2023-10-29 11:17:50 +0100

    added more information to logging

--- tools/config_handler.py
+++ tools/config_handler.py
@@ -9,12 +9,14 @@ with open("res/adv_config.json", "r") as json_file:
         adv_config = json.load(json_file)
 
 config_override_file = f'setup_helper/clients/{config["used_client"]}/config_overrides.json'
+
 if os.path.exists(config_override_file):
         with open(config_override_file, "r") as json_file: 
                 overrides = json.load(json_file)
                 config_overrides = overrides["config_overrides"]
                 adv_config_overrides = overrides["adv_config_overrides"]
         config.update(config_overrides)
+        print(config)
         adv_config.update(adv_config)
         
 def get_active_client_name(print_log : bool = True):
