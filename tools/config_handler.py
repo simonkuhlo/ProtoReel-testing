50d3daa643ac9db2abc16bffb3752bd1289b85c8
Simon <siga241@gmail.com>
2023-10-29 02:40:25 +0100

    realised config overrides by the used client

--- tools/config_handler.py
+++ tools/config_handler.py
@@ -1,4 +1,5 @@
 import json
+import os
 from tools import console_printer as p
 
 with open("res/config.json", "r") as json_file:
@@ -6,6 +7,15 @@ with open("res/config.json", "r") as json_file:
         
 with open("res/adv_config.json", "r") as json_file:
         adv_config = json.load(json_file)
+
+config_override_file = f'setup_helper/clients/{config["used_client"]}/config_overrides.json'
+if os.path.exists(config_override_file):
+        with open(config_override_file, "r") as json_file: 
+                overrides = json.load(json_file)
+                config_overrides = overrides["config_overrides"]
+                adv_config_overrides = overrides["adv_config_overrides"]
+        config.update(config_overrides)
+        adv_config.update(adv_config)
         
 def get_active_client_name():
         p.print_status("info", 4, f"Getting config: active client name")
